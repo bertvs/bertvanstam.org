58d0ecc08c2a9361c96859db266c58acbe9e4380
Bert <34618819+bertvs@users.noreply.github.com>
2022-02-12 20:01:30 +0100

    collapsible items in agenda

--- web/pages/agenda.php
+++ web/pages/agenda.php
@@ -3,13 +3,13 @@
 <h1>Agenda</h1>
 
 <h2>2022</h2>
-<h3>19 december &#183; 17.00 uur</h3>
+<h4>19 december &#183; 17.00 uur</h4>
 <p>Maranathakerk Den Haag<br />
 Samenzang en muziek in de sfeer van Kerst<br />
 Bert van Stam, orgel<br />
 Meer informatie volgt</p>
 
-<h3>10 maart 2022 &#183; 20.30 uur</h3>
+<h4>10 maart 2022 &#183; 20.30 uur</h4>
 <p>Kathedrale Basiliek Sint Bavo, Haarlem<br />
 Stilteconcert<br />
 met nieuw werk van <a href="https://soundcloud.com/kees-wisse" target="_blank" rel="noopener noreferrer">Kees Wisse</a><br />
@@ -17,27 +17,27 @@ Bas Duister, Ferdi Seelbach en Hans de Munnik, trompet<br />
 Bert van Stam, orgel<br />
 <a href="https://www.koepelkathedraal.nl/geloof-in-geluk/#agenda" target="_blank" rel="noopener noreferrer">Meer informatie en tickets</a></p>
 
-<h2>Archief - 2021</h2>
-
-<h3>20 april &#183; 12.45 uur</h3>
+<h2 class="collapsible">Archief 2021<small>&ensp;<i class="bi bi-chevron-down"></small></i></h2>
+<div style="display:none">
+<h4>20 april &#183; 12.45 uur</h4>
 <p>Laurenskerk Rotterdam<br />
 Orgelconcert<br />
 Bert van Stam, orgel<br />
 <a href="/agenda/2021-04-20">Meer informatie</a></p>
 
-<h3>14 augustus &#183; 15.00 uur</h3>
+<h4>14 augustus &#183; 15.00 uur</h4>
 <p>Grote of St. Michaelskerk, Oudewater<br />
 Zaterdagmiddagconcert<br />
 Wéndela van Swol, dwarsfluit en Bert van Stam, orgel<br />
 <a href="/agenda/2021-08-14">Meer informatie</a></p>
 
-<h3>15 augustus &#183; 17.00 uur</h3>
+<h4>15 augustus &#183; 17.00 uur</h4>
 <p>Maranathakerk, Den Haag<br />
 Zomerconcert<br />
 Wéndela van Swol, dwarsfluit en Bert van Stam, orgel<br />
 <a href="/agenda/2021-08-15">Meer informatie</a></p>
 
-<h3>1 oktober &#183; 15.30 uur</h3>
+<h4>1 oktober &#183; 15.30 uur</h4>
 <p>Maranathakerk, Den Haag<br />
 Nieuwe muziek voor het Maranatha-orgel<br />
 Studenten van het Koninklijk Conservatorium Den Haag, composities<br />
@@ -45,95 +45,112 @@ Guus Janssen, supervisie<br />
 Bert van Stam, orgel<br />
 <a href="/agenda/2021-10-01">Meer informatie</a></p>
 
-<h3>10 oktober &#183; 14.15 uur</h3>
+<h4>10 oktober &#183; 14.15 uur</h4>
 <p>Orgelpark Amsterdam<br />
 Componistenportret Klaas de Vries<br />
 'Boustrophedon' voor vier orgels, en andere werken</br>
 <a href="https://www.orgelpark.nl/nl/Agenda/Componistenportret%3A-Klaas-De-Vries@10-10-2021[14:15]" target="_blank" rel="noopener noreferrer">Meer informatie en tickets</a></p>
-
-<h2>2020</h2>
-
-<h3>11 oktober &#183; 17.00 uur</h3>
+</div>
+<h2 class="collapsible">Archief 2020<small>&ensp;<i class="bi bi-chevron-down"></small></i></h2>
+<div style="display:none">
+<h4>11 oktober &#183; 17.00 uur</h4>
 <p>Maranathakerk Den Haag<br />
 Duo-improvisatieconcert op joodse thema's<br />
 Gerben Mourik en Bert van Stam, orgel<br />
 <a href="/agenda/2020-10-11">Meer informatie</a></p>
 
-<h3>13 december &#183; 17.00 uur</h3>
+<h4>13 december &#183; 17.00 uur</h4>
 <p>Maranathakerk Den Haag<br />
 Muziek in de sfeer van Kerst<br />
 vocaal ensemble De Byrds o.l.v. Elodie Schuddeboom<br />
 Eduard Preda, piano</br />
 Bert van Stam, orgel<br /></p>
-
-<h2>2019</h2>
-
-<h3>31 maart &#183; 17.00 uur</h3>
+</div>
+<h2 class="collapsible">Archief 2019<small>&ensp;<i class="bi bi-chevron-down"></small></i></h2>
+<div style="display:none">
+<h4>31 maart &#183; 17.00 uur</h4>
 <p>Pfarrkirche, Diepoldsau (Zwitserland)<br />
 Lenten-concert<br />
 <a href="/agenda/2019-03-31">Meer informatie</a></p>
 
-<h3>4 mei &#183; 19.00 uur</h3>
+<h4>4 mei &#183; 19.00 uur</h4>
 <p>Maranathakerk, Den Haag<br />
 Dodenherdenking met muziek en declamatie<br />
 Aansluitend wandeling naar Vredespaleis</p>
 
-<h3>11 mei &#183; 15.00 uur</h3>
+<h4>11 mei &#183; 15.00 uur</h4>
 <p>Elandstraatkerk, Den Haag<br />
 Concert &#8216;de muzikale handtekening&#8217;<br />
 <a href="/agenda/2019-05-11">Meer informatie</a></p>
 
-<h3>8 juni &#183; 14.00 uur</h3>
+<h4>8 juni &#183; 14.00 uur</h4>
 <p>Maranathakerk, Den Haag<br />
 Presentatie CD 'Sprezzatura'<br />
 <a href="/agenda/2019-06-08">Meer informatie</a></p>
 
-<h3>17 augustus &#183; 15.00 uur</h3>
+<h4>17 augustus &#183; 15.00 uur</h4>
 <p>Kathedrale Basiliek Sint Bavo, Haarlem<br />
 Orgelconcert<br />
 <a href="/agenda/2019-08-17">Meer informatie</a></p>
 
-<h3>25 oktober &#183; 15.00 uur</h3>
+<h4>25 oktober &#183; 15.00 uur</h4>
 <p>Maranathakerk, Den Haag<br />
 &#8216;2000 jaar Arie&#8217; &#183; symposium ter gelegenheid van de voltooiing van de boekenreeks &#8216;Hymnologie&#8217; door dr. Arie Eikelboom</p>
 
-<h3>8 december &#183; 17.00 uur</h3>
+<h4>8 december &#183; 17.00 uur</h4>
 <p>Maranathakerk, Den Haag<br />
 Charlie Chaplin &#183; The Kid, met live orgelimprovisatie<br />
 Geerten Liefting en Bert van Stam, orgel</p>
 
-<h3>22 december &#183; 17.00 uur</h3>
+<h4>22 december &#183; 17.00 uur</h4>
 <p>Maranathakerk, Den Haag<br />
 Concert en samenzang: muziek in de sfeer van Kerst<br />
 m.m.v. vocaal ensemble De Byrds o.l.v. Elodie Schuddeboom</p>
-<h2>2018</h2>
-
-<h3>27 april &#183; 19.00 uur</h3>
+</div>
+<h2 class="collapsible">Archief 2018<small>&ensp;<i class="bi bi-chevron-down"></small></i></h2>
+<div style="display:none">
+<h4>27 april &#183; 19.00 uur</h4>
 <p>Stadtkirche, Glarus (Zwitserland)<br />
 Orgelconcert<br />
 <a href="/agenda/2018-04-27">Meer informatie</a></p>
 
-<h3>3 juni &#183; 17.00 uur</h3>
+<h4>3 juni &#183; 17.00 uur</h4>
 <p>Maranathakerk, Den Haag<br />
 Presentatieconcert<br />
 <a href="/agenda/2018-06-03">Meer informatie</a></p>
 
-<h3>7 september &#183; 16.00 uur</h3>
+<h4>7 september &#183; 16.00 uur</h4>
 <p>Maranathakerk, Den Haag<br />
 Muzikale bijdrage op Symposium &#8216;Heilige ruimte&#8217;</p>
 
-<h3>8 september &#183; 10.00-17.00 uur</h3>
+<h4>8 september &#183; 10.00-17.00 uur</h4>
 <p>Maranathakerk, Den Haag<br />
 Muzikale momenten tijdens openstelling kerk voor Open Monumentendag</p>
 
-<h3>15 september &#183; 16.00 uur</h3>
+<h4>15 september &#183; 16.00 uur</h4>
 <p>Citykerk Het Steiger, Rotterdam<br />
 Orgelconcert<br />
 <a href="/agenda/2018-09-15">Meer informatie</a></p>
 
-<h3>23 december &#183; 17.00 uur</h3>
+<h4>23 december &#183; 17.00 uur</h4>
 <p>Maranathakerk, Den Haag<br />
 Concert en samenzang: muziek in de sfeer van Kerst<br />
 m.m.v. vocaal ensemble De Byrds o.l.v. Elodie Schuddeboom</p>
+</div>
+<script>
+    let coll = document.getElementsByClassName("collapsible");
+    let i;
 
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
+    }
+</script>
 <?php require 'footer.php'; ?>
